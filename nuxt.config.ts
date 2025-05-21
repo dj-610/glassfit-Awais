8d91dcb4b2a5368a30a4a295f017afeb7364970c
dj-610 <63324047+dj-610@users.noreply.github.com>
2025-05-21 23:20:37 +0500

    Add Github Pages Deployment workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
+  ssr:false,
+  app: {
+    baseURL: '/glassfit-Awais/',
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css:['~/assets/css/main.css'],
